46491bbc632c553ceb9ce5172c3704914cfd6723
flare <flare.the.protogen.yt@gmail.com>
2025-05-19 16:01:26 -0600

    cleanup

--- types/cc/require.d.ts
+++
@@ -1,5 +0,0 @@
-/** @noSelfInFile **/
-/** @noResolution **/
-declare module "cc.require" {
-    export function make(env: Object|LuaTable, dir: string): LuaMultiReturn<[(name: string) => any, Object|LuaTable]>;
-}
